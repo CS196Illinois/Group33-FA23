5f25ebaa389b7bbe6c7d6d1b4e0b96ceda370d1d
wgarrett707 <112145376+wgarrett707@users.noreply.github.com>
2023-10-11 19:19:01 -0500

    added images to events and search bar functionality

--- Project/Frontend/Events/events.js
+++ Project/Frontend/Events/events.js
@@ -1,40 +1,52 @@
 var data = {
     0: {
-        title: "Event 1",
-        subtitle: "Good Event Upcoming",
-        desc: "blah blah blah"
+        title: "Helping Starving Children",
+        subtitle: "Visiting local poor neighborhoods to distribute food",
+        desc: "Food/drink will be provided."
     },
     1: {
-        title: "Event 2",
-        subtitle: "Good Event Upcoming",
-        desc: "blah blah blah"
+        title: "Food Pantry Visit",
+        subtitle: "Help us pack food baskets for the homeless!",
+        desc: "Please sign up below, first 20 volunteers get a tote bag!"
     },
     2: {
-        title: "Event 2",
-        subtitle: "Good Event Upcoming",
-        desc: "blah blah blah"
+        title: "Soup Kitchen Visit",
+        subtitle: "Serve soup to impoverished communities",
+        desc: "Bring your own set of gloves please."
     },
     3: {
-        title: "Event 3",
-        subtitle: "Good Event Upcoming",
-        desc: "blah blah blah"
-    },
-    4: {
         title: "Event 4",
         subtitle: "Good Event Upcoming",
         desc: "blah blah blah"
     },
-    5: {
+    4: {
         title: "Event 5",
         subtitle: "Good Event Upcoming",
         desc: "blah blah blah"
     },
-    6: {
+    5: {
         title: "Event 6",
+        subtitle: "Good Event Upcoming",
+        desc: "blah blah blah"
+    },
+    6: {
+        title: "Event 7",
         subtitle: "Good Event Upcoming",
         desc: "blah blah blah"
     }
 }
+
+var searchBar = document.getElementById("search")
+
+searchBar.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    for (let i = 0; i < Object.keys(data).length; i++) {
+        console.log(data[i].title)
+        var shouldShow = data[i].title.toLowerCase().includes(value) || data[i].subtitle.toLowerCase().includes(value) || data[i].desc.toLowerCase().includes(value)
+        var card = document.getElementsByClassName("entryNum" + i)[0]
+        card.classList.toggle("hide", !shouldShow)
+    }
+}) 
 
 function init() {
     var height = screen.height
@@ -46,11 +58,12 @@ function init() {
         var entry = data[i]
         if (i == 0) {
             list.innerHTML += `
-        <div class="card eventCard" id = "firstEvent">
+        <div class="card eventCard entryNum${i}" id = "firstEvent">
             <div class="card-header">
                 ${entry.title}
             </div>
             <div class="card-body">
+                <img class="float-left img-thumbnail mr-3" style="width: 16cqw; height: 9cqw;" src="./sample1.jpg">
                 <h5 class="card-title">${entry.subtitle}</h5>
                 <p class="card-text">${entry.desc}</p>
                 <a href="../Apply/apply.html" class="btn btn-primary">Sign up!</a>
@@ -60,11 +73,12 @@ function init() {
             continue
         }
         list.innerHTML += `
-        <div class="card eventCard">
+        <div class="card eventCard entryNum${i}">
             <div class="card-header">
                 ${entry.title}
             </div>
             <div class="card-body">
+            <img class="float-left img-thumbnail mr-3" style="width: 16cqw; height: 9cqw;" src="./sample1.jpg">
                 <h5 class="card-title">${entry.subtitle}</h5>
                 <p class="card-text">${entry.desc}</p>
                 <a href="../Apply/apply.html" class="btn btn-primary">Sign up!</a>
