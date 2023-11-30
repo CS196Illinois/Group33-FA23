e384128f5af6033c6cde7bb7881b9f257889e8b7
wrjodjana <141975732+wrjodjana@users.noreply.github.com>
2023-11-29 22:59:02 -0600

    cleaned up donationform.js

--- Project/Frontend/Donate/donation-form.js
+++ Project/Frontend/Donate/donation-form.js
@@ -1,8 +1,8 @@
 function changeMethod(method) {
-    let formContent = "";
-    switch (method) {
-        case 'card':
-            formContent = `
+  let formContent = "";
+  switch (method) {
+    case "card":
+      formContent = `
             <h4>Card information</h4>
             <form>
                 <div class="form-group">
@@ -22,26 +22,26 @@ function changeMethod(method) {
                     <input type="email" class="form-control" placeholder="example@email.com">
                 </div>
             </form>`;
-            break;
+      break;
 
-        case 'bank':
-            formContent = `
+    case "bank":
+      formContent = `
             <h4>Internet Banking</h4>
             <form>
                 <div class="form-group">
                     <label>Select Your Bank</label>
                     <select class="form-control">
-                        <option>Bank A</option>
-                        <option>Bank B</option>
-                        <option>Bank C</option>
+                        <option>Bank of America</option>
+                        <option>Chase Bank</option>
+                        <option>PNC</option>
                     </select>
                 </div>
                 <button type="button" class="btn btn-primary btn-block">Continue to Bank</button>
             </form>`;
-            break;
+      break;
 
-        case 'apple_google':
-            formContent = `
+    case "apple_google":
+      formContent = `
             <h4>Apple/Google Pay</h4>
             <div class="text-center">
                 <button type="button" class="btn btn-outline-dark btn-lg mb-2" onclick="window.location.href='https://www.apple.com/apple-pay/'">
@@ -52,77 +52,75 @@ function changeMethod(method) {
                     <i class="fab fa-google-pay"></i> Pay with Google Pay
                 </button>
             </div>`;
-            break;
-    }
-    document.getElementById('paymentForm').innerHTML = formContent;
+      break;
+  }
+  document.getElementById("paymentForm").innerHTML = formContent;
 }
 
 async function isLoggedIn() {
-    const token = localStorage.getItem("token")
-    const userName = localStorage.getItem("userName")
-    if (token == null || userName == null) {
-        return false
-    }
-    try {
-        const response = await fetch('http://localhost:3000/auth', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-              },
-            body: JSON.stringify({
-                user:userName,
-                token:token
-            })
-        })
+  const token = localStorage.getItem("token");
+  const userName = localStorage.getItem("userName");
+  if (token == null || userName == null) {
+    return false;
+  }
+  try {
+    const response = await fetch("http://localhost:3000/auth", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: userName,
+        token: token,
+      }),
+    });
 
-        const result = await response
-        if (result.status == 200) {
-            return true
-        } else {
-            return false
-        }
-        
-    } catch (error) {
-        console.error(error)
-        return false
+    const result = await response;
+    if (result.status == 200) {
+      return true;
+    } else {
+      return false;
     }
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
 }
 
 async function logout(event) {
-    event.preventDefault()
-    const userName = localStorage.getItem("userName")
+  event.preventDefault();
+  const userName = localStorage.getItem("userName");
 
-    try {
-        const response = await fetch('http://localhost:3000/logout', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-              },
-            body: JSON.stringify({
-                user:userName
-            })
-        })
+  try {
+    const response = await fetch("http://localhost:3000/logout", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: userName,
+      }),
+    });
 
-        const result = await response
-        if (result.status == 200) {
-            window.location.replace("../Home/home.html");
-        } else {
-            console.log("Failed to logout")
-        }
-        
-    } catch (error) {
-        console.error(error)
+    const result = await response;
+    if (result.status == 200) {
+      window.location.replace("../Home/home.html");
+    } else {
+      console.log("Failed to logout");
     }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 function init() {
-    isLoggedIn().then(loggedIn=>{
-        if (loggedIn) {
-            const loginLink = document.getElementById("loginLink")
-            loginLink.innerHTML = "Logout"
-            loginLink.setAttribute("href","")
-            loginLink.onclick = logout
-        }
-    })
+  isLoggedIn().then((loggedIn) => {
+    if (loggedIn) {
+      const loginLink = document.getElementById("loginLink");
+      loginLink.innerHTML = "Logout";
+      loginLink.setAttribute("href", "");
+      loginLink.onclick = logout;
+    }
+  });
 }
-window.onload = init
+window.onload = init;
